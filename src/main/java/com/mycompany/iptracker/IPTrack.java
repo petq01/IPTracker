b67ca2ca2b3ccec1ceeee244384b6658d526c610
petya <petya.marinova@markovskisolutions.com>
2017-08-23 17:52:53 +0300

    simple example commands

---
+++ src/main/java/com/mycompany/iptracker/IPTrack.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.iptracker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Petya
+ */
+public class IPTrack {
+
+    public static void main(String[] args) throws Exception {
+        Date date = new Date();
+        List<String> ipNames = new ArrayList<>();
+
+        Process arpProcess = Runtime.getRuntime().exec("arp -a");
+
+        BufferedReader brArp = new BufferedReader(new InputStreamReader(arpProcess.getInputStream()));
+
+        // String out is used to store output of this command(process)
+        String out = "";
+
+        while (true) {
+            String l = null;
+            try {
+                l = brArp.readLine();
+            } catch (IOException ex) {
+            }
+            if (l == null) {
+                break;
+            }
+            out += "\n" + l;
+        }
+
+        // A compiled representation of a regular expression
+        Pattern pattern
+                = Pattern.compile(".*\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b");
+
+        /* An engine that performs match operations on a character sequence by interpreting a Pattern */
+        Matcher match = pattern.matcher(out);
+
+        out = "";
+        String prev = "", pLoc;
+
+        if (!(match.find())) // In case no IP address Found in out
+        {
+            out = "No IP found!";
+        } else {
+
+            /* Returns the input subsequence matched by the previous match in this case IP of our interface */
+            pLoc = match.group();
+
+            out += pLoc + "\nOther Hosts'(In Same Network) IP addresses:\n";
+            while (match.find()) {
+                pLoc = match.group();	// Returns the IP of other hosts
+                ipNames.add(pLoc);
+
+                out += pLoc + "\n";
+            }
+            try {
+                brArp.close();
+            } catch (IOException ex) {
+            }
+        }
+        System.out.println("\n ------------------------ \n\n " + date + "\n\n\n");
+        for (String ip : ipNames) {
+
+            Process nsProcess = Runtime.getRuntime().exec("nslookup " + ip);
+            BufferedReader brNs = new BufferedReader(new InputStreamReader(nsProcess.getInputStream()));
+
+            Process physicalAddr = Runtime.getRuntime().exec("arp -a " + ip);
+            BufferedReader brPsyhical = new BufferedReader(new InputStreamReader(physicalAddr.getInputStream()));
+            String l = null;
+            String a = null;
+            printCommand(l, brNs);
+            printCommand(a, brPsyhical);
+
+        }
+
+    }
+
+    public static void printCommand(String l, BufferedReader br) {
+        while (true) {
+            try {
+                l = br.readLine();
+            } catch (IOException ex) {
+            }
+            System.out.println(l);
+            if (l == null) {
+                System.out.println("----");
+                break;
+            }
+        }
+        try {
+            br.close();
+        } catch (IOException ex) {
+        }
+    }
+
+}
